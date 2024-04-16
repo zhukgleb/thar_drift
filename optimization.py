1cbbb0961f77905e644b469a5e23f68a5e7094e9
zhuklevich gleb <zhuklevich.gleb@gmail.com>
2024-04-16 18:22:52 +0300

    feat: pre-solution image optimization

---
+++ optimization.py
@@ -0,0 +1,130 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+from scipy import ndimage as ndi
+from skimage import filters
+from skimage import transform
+import os
+from skimage import data, color
+from astropy.io import fits
+
+
+# astronaut = color.rgb2gray(data.astronaut())
+# shifted = ndi.shift(astronaut, (30, 30))
+
+fits_list = os.listdir("data/")
+print(fits_list)
+data_list = []
+
+for i in range(len(fits_list)):
+    with fits.open("data/" + fits_list[i]) as hdul:
+        data_list.append(hdul[0].data[0])
+
+rep_fits = data_list[0]
+shifted = data_list[1]
+# shifted = ndi.shift(rep_fits, (30, 30))
+
+def mse(arr1, arr2):
+    """Compute the mean squared error between two arrays."""
+    return np.mean((arr1 - arr2)**2)
+
+
+def rep_fits_shift_error(shift, image):
+    corrected = ndi.shift(image, (0, shift))
+    return mse(rep_fits, corrected)
+
+
+def downsample2x(image):
+    offsets = [((s + 1) % 2) / 2 for s in image.shape]
+    slices = [slice(offset, end, 2)
+              for offset, end in zip(offsets, image.shape)]
+    coords = np.mgrid[slices]
+    return ndi.map_coordinates(image, coords, order=1)
+
+
+def gaussian_pyramid(image, levels=6):
+    """Make a Gaussian image pyramid.
+
+    Parameters
+    ----------
+    image : array of float
+        The input image.
+    max_layer : int, optional
+        The number of levels in the pyramid.
+
+    Returns
+    -------
+    pyramid : iterator of array of float
+        An iterator of Gaussian pyramid levels, starting with the top
+        (lowest resolution) level.
+    """
+    pyramid = [image]
+
+    for level in range(levels - 1):
+        blurred = ndi.gaussian_filter(image, sigma=2/3)
+        image = downsample2x(image)
+        pyramid.append(image)
+
+    return reversed(pyramid)
+
+
+def make_rigid_transform(param):
+    r, tc, tr = param
+    return transform.SimilarityTransform(rotation=r,
+                                         translation=(tc, tr))
+
+
+def cost_mse(param, reference_image, target_image):
+    transformation = make_rigid_transform(param)
+    transformed = transform.warp(target_image, transformation, order=3)
+    return mse(reference_image, transformed)
+
+
+def align(reference, target, cost=cost_mse, nlevels=7, method='Powell'):
+    pyramid_ref = gaussian_pyramid(reference, levels=nlevels)
+    pyramid_tgt = gaussian_pyramid(target, levels=nlevels)
+
+    levels = range(nlevels, 0, -1)
+    image_pairs = zip(pyramid_ref, pyramid_tgt)
+
+    p = np.zeros(3)
+
+    for n, (ref, tgt) in zip(levels, image_pairs):
+        p[1:] *= 2
+        if method.upper() == 'BH':
+            res = optimize.basinhopping(cost, p,
+                                        minimizer_kwargs={'args': (ref, tgt)})
+            if n <= 4:  # avoid basin-hopping in lower levels
+                method = 'Powell'
+        else:
+            res = optimize.minimize(cost, p, args=(ref, tgt), method='Powell')
+        p = res.x
+        # print current level, overwriting each time (like a progress bar)
+        print(f'Level: {n}, Angle: {np.rad2deg(res.x[0]) :.3}, '
+              f'Offset: ({res.x[1] * 2**n :.3}, {res.x[2] * 2**n :.3}), '
+              f'Cost: {res.fun :.3}', end='\r')
+
+    print('')  # newline when alignment complete
+    return make_rigid_transform(p)
+
+
+from skimage import util
+
+theta = 60
+# rotated = transform.rotate(rep_fits, theta)
+# rotated = util.random_noise(rotated, mode='gaussian',
+#                             seed=0, mean=0, var=1e-3)
+
+tf = align(rep_fits, shifted)
+corrected = transform.warp(shifted, tf, order=3)
+
+f, (ax0, ax1, ax2) = plt.subplots(1, 3)
+ax0.imshow(rep_fits)
+ax0.set_title('Original')
+ax1.imshow(shifted)
+ax1.set_title('Rotated')
+ax2.imshow(corrected)
+ax2.set_title('Registered')
+for ax in (ax0, ax1, ax2):
+    ax.axis('off')
+plt.show()
